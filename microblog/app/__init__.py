0b62b0c312ed291a6a34af5009dc2e699d4c374a
Jack Da Tsai <73694355+JackDaTsai@users.noreply.github.com>
2024-02-27 12:59:19 +0800

    Update import statement in __init__.py

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from microblog.config import Config
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
